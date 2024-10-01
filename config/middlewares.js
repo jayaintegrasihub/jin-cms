e5c35d66475919cabd3d5cddbb23b72937c95add
Xyzting <reyhan.fathir1110@gmail.com>
2024-10-01 09:30:13 +0700

    refactor: config middleware cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://172.168.1.59:5173",
+        "http://172.168.1.59",
+      ],
+    },
+  },
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
