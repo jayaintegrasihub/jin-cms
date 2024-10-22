7d2b1cbf926f287316bf70245c3ccdf7dad54143
ichaspg <ichaspurnama12@gmail.com>
2024-10-22 08:21:25 +0700

    fix : cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,6 +14,7 @@ module.exports = [
         "http://172.19.0.1:5173",
         "http://172.22.80.1:5173",
         "https://jin-compro-fe.vercel.app",
+        "https://jayaintegrasi.id",
       ],
     },
   },
