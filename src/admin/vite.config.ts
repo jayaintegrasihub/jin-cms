f22f7a8bb54ce2e6230efc36eb93338f571f29a8
ichaspg <ichaspurnama12@gmail.com>
2025-10-01 13:41:59 +0700

    fix : add vite config

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, mergeConfig } from "vite";
+
+export default (config) => {
+  return mergeConfig(
+    config,
+    defineConfig({
+      resolve: {
+        alias: {
+          "@": "/src",
+        },
+      },
+      server: {
+        allowedHosts: true,
+      },
+    }),
+  );
+};
